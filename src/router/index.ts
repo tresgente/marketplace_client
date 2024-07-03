4ffc8b00b3a732c3aa8f40b54b5782d089572c47
BHUSHAN RAMTEKE <bhushan7668@gmail.com>
2024-07-03 21:58:01 +0530

    start with dailyui setup

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+// import { createRouter, createWebHistory } from 'vue-router';
+import App from '../App.vue';
+// import Home from '../views/Home.vue';
+// import Login from '../views/Login.vue';
+
+const routes = [
+  { path: '/', component: App },
+  //   { path: '/login', component: Login },
+];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
+// export default router;
